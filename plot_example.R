04c5d8665fa4df2c401dd1ea0bf84c78804c889e
Victoria <victoria.ruiz.serra@gmail.com>
2015-11-03 13:00:34 +0100

    Update plot_example.R

--- plot_example.R
+++ plot_example.R
@@ -1,33 +1,48 @@
 
- significance<-tukey.plot.significance(data) 
+  library(ggplot2)
+   library(grid)
+   library(fBasics)
+   library(tidyr)
+   library(reshape)
+   library(agricolae)
+   library(Rmisc)
+   library(gdata)
+   library(rlist)
+
+ load("myFunction.Rdata")
  
+ data<- read.csv(file.choose()  , header=T, sep=";")
+ significance<-tukey.plot.significance(data)
  tukey_letters<-significance$tukey_letters
  stats<-significance$stats
- geom.path<-significance$geom.path
 
  
-   offset<-0.02
-   offset_segment<-10
-   
-   p<- ggplot(stats, aes(x = variable, y = value, fill = group)) + 
-   geom_bar(stat = "identity", position = "dodge", colour="black",width=.7,size=.5)+ 
-   facet_wrap(~ group)+
-   geom_errorbar(aes(ymin=value-se, ymax=value+se), width=.1,size=.5)+
-   geom_line(size=.8)+
-   theme(plot.title = element_text(lineheight=.8, face="bold"),
-   strip.text.x = element_text(size = 20, face="bold"),axis.text.x = element_text(angle=45, vjust=1))+
-   geom_text(data =tukey_letters, aes(x=xpos.t, y=ymax+offset, 
-              label= M), size = 5)
-              
-## If you are interested in setting segments and asteriks just use geom_segmet() + geom_text() with information
-## from significance$geom.path 
-    geom_text(data = geom.path, aes(x=xmed, y=ymax+offset_segment, 
-              label=sig., size = 8)+
-    geom_segment(data = geom.path, size = .8, 
-                 aes(x=x1, y=ymax, xend=x2, yend=ymax+offset_segment)+   ##horizontal line
-    geom_segment(data = geom.path, size = .8, 
-                 aes(x=x1, y=ymax, xend=x1, yend=ymax+offset_segment))+   ## vertical line
-    geom_segment(data = geom.path, size = .8, 
-                 aes(x=x2, y=ymax, xend=x2, yend=ymax+offset_segment))   ##vertical line          
-              
+
+offset<-7
+offset_asterisk<-10 
   
+plot<- ggplot( stats, aes(x = variable, y = value, fill = group)) + 
+         geom_bar(stat = "identity", position =position_dodge(),colour="black",width=.7,size=.5)+ 
+         geom_errorbar(aes(ymin=value-se, ymax=value+se), width=.1,size=.5,position=position_dodge(.7))+
+         geom_line(size=.8)+
+         theme(
+               plot.title = element_text(lineheight=.8, face="bold",size=10),
+               axis.title = element_text(size =8, face="bold"),
+               axis.text = element_text(angle=45, vjust=1,size=8,face="bold"),
+               axis.ticks = element_line(size = rel(2.5)),
+               axis.ticks.length = unit(0.5, "cm")
+               )+
+         labs(
+              title="your title",
+              x="x axis title",
+              y="y axis title"
+              )+
+         scale_x_discrete(
+                          limit = c("names of x variables"),
+                          labels = c("new names of x variables")
+                          )+
+         geom_text(data =tukey_letters, 
+                     aes(x=xpos.t, y=ymax+offset_asterisk,label=M), 
+                     size = 5,position=position_dodge(.5)
+                    )+
+         facet_wrap(~group) ## You can use face_wrap function only if you need it
