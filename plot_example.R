9fe95816db13ea3944da92a2eae2b604b0ec998b
Victoria <victoria.ruiz.serra@gmail.com>
2018-12-01 22:49:06 +0100

    Update plot_example.R

--- plot_example.R
+++ plot_example.R
@@ -1,29 +1,27 @@
+###################################################################################
+# Load tukey significance function
+###################################################################################
 
-  library(ggplot2)
-   library(grid)
-   library(fBasics)
-   library(tidyr)
-   library(reshape)
-   library(agricolae)
-   library(Rmisc)
-   library(gdata)
-   library(rlist)
+load("tukeySignificanceLettersFunction.Rdata")
 
- load("myFunction.Rdata")
- 
- data<- read.csv(file.choose()  , header=T, sep=";")
- significance<-tukey.plot.significance(data)
- tukey_letters<-significance$tukey_letters
- stats<-significance$stats
+###################################################################################
+# Load data example and apply function
+###################################################################################
+data_example <- read.table("./data_example.csv", sep =";", header=T)
 
- 
+significance<-tukey_plot_significance(data_example) 
 
+# Extract letters positions
+tukey_letters<-significance$tukey_letters
+stats<-significance$stats
+
+# Set asteriks position (so it won't fall on top of the error bars)
 offset<-7
 offset_asterisk<-10 
   
-plot<- ggplot( stats, aes(x = variable, y = value, fill = group)) + 
+plot<- ggplot( tukey_letters, aes(x = variable, y = value.x, fill = L1)) + 
          geom_bar(stat = "identity", position =position_dodge(),colour="black",width=.7,size=.5)+ 
-         geom_errorbar(aes(ymin=value-se, ymax=value+se), width=.1,size=.5,position=position_dodge(.7))+
+         geom_errorbar(aes(ymin=value.x-se, ymax=value.x+se), width=.1,size=.5,position=position_dodge(.7))+
          geom_line(size=.8)+
          theme(
                plot.title = element_text(lineheight=.8, face="bold",size=10),
@@ -37,12 +35,9 @@ plot<- ggplot( stats, aes(x = variable, y = value, fill = group)) +
               x="x axis title",
               y="y axis title"
               )+
-         scale_x_discrete(
-                          limit = c("names of x variables"),
-                          labels = c("new names of x variables")
-                          )+
-         geom_text(data =tukey_letters, 
-                     aes(x=xpos.t, y=ymax+offset_asterisk,label=M), 
+        facet_wrap(~L1)+ ## You can use face_wrap function only if you need it+
+        geom_text(data =tukey_letters, 
+                     aes(x=xpos, y=ymax+offset_asterisk,label=groups), 
                      size = 5,position=position_dodge(.5)
-                    )+
-         facet_wrap(~group) ## You can use face_wrap function only if you need it
+                    )
+plot
